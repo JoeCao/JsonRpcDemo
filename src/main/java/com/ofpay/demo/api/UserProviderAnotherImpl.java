83e98ceaed19b85aaa3e31504e3bedf592ddcb4f
曹祖鹏 <chinalibra@163.com>
2015-04-13 12:19:02 +0800

    记录log

--- src/main/java/com/ofpay/demo/api/UserProviderAnotherImpl.java
+++ src/main/java/com/ofpay/demo/api/UserProviderAnotherImpl.java
@@ -1,5 +1,8 @@
 package com.ofpay.demo.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,19 +11,25 @@ import java.util.Map;
  */
 public class UserProviderAnotherImpl implements UserProvider {
 
+    Logger logger = LoggerFactory.getLogger(getClass());
+
     public boolean isLimit(Gender gender, String name) {
+        logger.info(String.format("input gender=%sand name=%s", gender, name));
         return Gender.MAN == gender;
     }
 
     public User getUser(String userId) {
+        logger.info("input userId = " + userId);
         return new User(userId, "Joe", 48);
     }
 
     public User queryUser(User user) {
+        logger.info("input user = " + user);
         return new User(user.getId(), "get:" + user.getName(), user.getAge() + 18);
     }
 
     public Map<String, User> queryAll() {
+        logger.info("input");
         Map<String, User> map = new HashMap<String, User>();
         map.put("001", new User("001", "Joe", 18));
         map.put("002", new User("002", "Wen", 20));
